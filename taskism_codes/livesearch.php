8afaaaa405e0f028aee095883351578c267afb68
yurag_I <nasheryurag7289@gmail.com>
2022-11-29 13:41:30 +0800

    edited searchbar backend, still not final

--- taskism_codes/livesearch.php
+++ taskism_codes/livesearch.php
@@ -5,41 +5,44 @@ if (isset($_POST['input'])) {
 
     $input = $_POST['input'];
 
-    $myQuery = "SELECT * FROM users WHERE fName LIKE '{$input}%' OR lName LIKE '{$input}%' OR Gender LIKE '{$input}%' LIMIT 3";
+    $myQuery = "SELECT * FROM users WHERE statuss LIKE '{$input}%' LIMIT 3";
 
     $result = mysqli_query($con, $myQuery);
 
-    if (mysqli_num_rows($result) > 0) {?>
-        <table style="padding:20px;">
+    if (mysqli_num_rows($result) > 0) { ?>
+        <table style="padding-top:20px">
             <!-- <thead>
                 <tr>
-                <th>UserID</th>
-                <th>fName</th>
-                <th>lName</th>
-                <th>Gender</th>
+                    <th style="font-size:5mm;color: #6C8054">First Name</th>
+                    <th style="font-size:5mm;color: #6C8054">Last Name</th>
+                    <th style="font-size:5mm;color: #6C8054">Gender</th>
+                    <th style="font-size:5mm;color: #6C8054">Status</th>
                 </tr>
             </thead> -->
-
             <tbody>
                 <?php
-while ($row = mysqli_fetch_assoc($result)) {
-        $fname = $row['fName'];
-        $lname = $row['lName'];
-        $gender = $row['Gender'];
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $fname = $row['fName'];
+                    $lname = $row['lName'];
+                    $gender = $row['Gender'];
+                    $status = $row['statuss'];
 
-        ?>
-                    <tr>
-                        <td><?php echo $fname; ?></td>
-                        <td><?php echo $lname; ?></td>
-                        <td><?php echo $gender; ?></td>
+                ?>
+                    <tr style="line-height:5%;">
+                        <td style="color:black ;font-family:Quicksand"><?php echo "<input type='checkbox'/>"; ?></td>
+                        <td style="color:black ;font-family:Quicksand"><?php echo $fname; ?></td>
+                        <td style="color:black ;font-family:Quicksand"><?php echo $lname; ?></td>
+                        <td style="color:black ;font-family:Quicksand"><?php echo $gender; ?></td>
+                        <td style="color:black ;font-family:Quicksand"><?php echo $status; ?></td>
+                        <td style="color:black ;font-family:Quicksand"><?php echo "<a href='' style=margin-left:220%>View</a>"; ?></td>
                     </tr>
-                    <?php
-}
-        ?>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
-        <?php
-} else {
+<?php
+    } else {
         echo "No data is found!";
     }
 }
